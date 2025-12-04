016a17e4147712c8f17e9d06c19421168d2f6354
Jims42 <jimmycoco.42@outlook.fr>
2025-12-04 09:45:37 +0100

    exo

---
+++ Intro/cours-Eudes/Exercices/ex1.java
@@ -0,0 +1,18 @@
+public class ex1 {
+
+    public static void main(String[] args) {
+        
+        int a = 8;
+        int b = 7;
+        int c = 3;
+        int s = 56;
+
+
+        System.out.println("resultat A: 8x7 le tous divise par 3 = " +s/+c);
+        System.out.println("resultat B: (8x7)/3 = " +a*+b/+c);
+
+    }
+
+      
+
+    }
---
+++ Intro/cours-Eudes/Exercices/ex2.java
@@ -0,0 +1,12 @@
+public class ex2 {
+
+    public static void main(String[] args) {
+        
+        boolean A = true;
+        boolean B = true;
+        boolean C = false;
+
+        System.out.println("A ou B et non(C) = "+ (A || B && !C)  );
+    }
+    
+}
