4884d400f6119ca299c921beddb4f4643b712e13
Jims42 <jimmycoco.42@outlook.fr>
2025-12-18 07:30:17 +0100

    exo

--- Intro/cours-Eudes/Jalon_blanc_02.java
+++ Intro/cours-Eudes/Jalon_blanc_02.java
@@ -5,14 +5,24 @@ public class Jalon_blanc_02 {
         Scanner clavier = new Scanner(System.in);
 
         int bouteille=100;
-        int bu;
-        int reste=0;
+        
+       
+        while (bouteille>0){
+       
+        System.out.printf("Vous avez \u001B[33m %d \u001B[0m cl dans votre bouteille,\n Combien de centilitre voulez vous boire ? \n",bouteille);
+        int bu=clavier.nextInt();
 
-        System.out.println("Vous avez \u001B[31m"+reste+"\u001B[0m cl dans votre bouteille,\n Combien de centilitre voulez vous boire ? ");
-        bu=clavier.nextInt();
+        if (bu<=bouteille) { bouteille-=bu;
+        }else{System.out.println("Vous ne pouvez pas boire plus de ce qu'il reste dans la bouteille");}
 
-        reste=bouteille-bu;
-
+        if (bouteille>0) {
+            System.out.printf("Il reste encore\u001B[32m %d \u001B[0m cl dans la bouteille.\n",bouteille);  
+        }else{
+            System.out.println("Vous avez tous bu,\n\u001B[31mLa bouteille est vide.\u001B[0m");
+        }
+        
+         }
+clavier.close();
 
     }
 }
