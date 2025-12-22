261f904e978055220f46f7330bc1c084b6c35172
Jims42 <jimmycoco.42@outlook.fr>
2025-12-22 05:38:08 +0100

    test

--- Intro/cours-Eudes/Jalon_blanc_03.java
+++ Intro/cours-Eudes/Jalon_blanc_03.java
@@ -4,27 +4,27 @@ public class Jalon_blanc_03 {
   public static void main(String[] args) {
     Scanner clavier = new Scanner(System.in);
 
-    // Demander à l'utilisateur s'il veut lancer le compte à rebours
-    // System.out.println("Voulez-vous lancer le compte à rebours ? (true/false)");
-    // boolean lancerCompteARebours = scanner.nextBoolean();
+    //  Demander à l'utilisateur s'il veut lancer le compte à rebours
+    //  System.out.println("Voulez-vous lancer le compte à rebours ? (true/false)");
+    //  boolean lancerCompteARebours = scanner.nextBoolean();
 
-    // if (lancerCompteARebours) {
-    // Compte à rebours de 10 à 0
-    // for (int i = 10; i >= 0; i--) {
-    // System.out.println(i);
-    // try {
-    // Pause d'une seconde entre chaque nombre
-    // Thread.sleep(1000);
-    // } catch (InterruptedException e) {
-    // e.printStackTrace();
-    // }
-    // }
-    // System.out.println("Bonne année 2026 !!");
-    // } else {
-    // System.out.println("Compte à rebours annulé !");
-    // }
+    //  if (lancerCompteARebours) {
+    //  Compte à rebours de 10 à 0
+    //  for (int i = 10; i >= 0; i--) {
+    //  System.out.println(i);
+    //  try {
+    //  Pause d'une seconde entre chaque nombre
+    //  Thread.sleep(1000);
+    //  } catch (InterruptedException e) {
+    //  e.printStackTrace();
+    //  }
+    //  }
+    //  System.out.println("Bonne année 2026 !!");
+    //  } else {
+    //  System.out.println("Compte à rebours annulé !");
+    //  }
 
-    // scanner.close();
+    //  scanner.close();
     boolean question;
     int debut = 10;
 
